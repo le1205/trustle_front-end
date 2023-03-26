b3323040355e758aa8054c7b2e9e3b5d379ffdde
oldhand7 <oldhanddev@gmail.com>
2023-03-26 11:50:34 -0400

    privady desktop

--- src/pages/Privacy/Privacy.tsx
+++ src/pages/Privacy/Privacy.tsx
@@ -2,8 +2,30 @@ import React from "react";
 
 const Privacy = () => {
   return (
-    <>
-    </>
+    <section className="px-[42px] lg:pl-[128px] lg:pr-[113px] flex justify-center mb-[30px]">
+      <div className="max-w-[1440px] w-full">
+        <h1 className="text-black text-[19.73px] lg:text-[32.4043px] leading-[25px] lg:leading-[49px] font-[400] font-bold mt-[50px] lg:mt-0]">Privacy</h1>
+        <div className="pl-0 lg:pl-[14px] mt-[25px] lg:mt-[38px] gap-[20px] flex flex-col">
+          <p className="text-black text-[19.35px] lg:text-[24.7907px] leading-[29px] lg:leading-[37px] font-[400] font-regular">At Trustle we are committed to protecting the privacy of our users and their personal information. This Privacy Policy explains how we collect, use, and disclose the personal information of our users, and what rights and choices they have in relation to their personal information.</p>
+          <p className="text-black text-[19.35px] lg:text-[24.7907px] leading-[29px] lg:leading-[37px] font-[400] font-regular"><span className="ml-[15px]">1.Collection of Personal Information</span> <br />We collect personal information from our users when they sign up for an account, complete their profile, and use our services. The personal information we collect may include, but is not limited to, the user's name, email address, social security number, and online account information.</p>
+          <p className="text-black text-[19.35px] lg:text-[24.7907px] leading-[29px] lg:leading-[37px] font-[400] font-regular"><span className="ml-[15px]">2. Use of Personal Information</span> <br />We use the personal information of our users to provide and improve our services, to communicate with our users, and to comply with legal requirements. The personal information we collect will not be used for any other purpose unless the user gives their explicit consent.</p>
+          <p className="text-black text-[19.35px] lg:text-[24.7907px] leading-[29px] lg:leading-[37px] font-[400] font-regular">
+            <span className="ml-[15px]">3. Disclosure of Personal Information</span> <br />We may disclose personal information to third parties in the following circumstances:
+            <ul className="list-disc ml-[40px]">
+              <li>To comply with legal requirements</li>
+              <li>To enforce our terms and conditions</li>
+              <li>To protect the rights, property, or safety of Trustle, our users, or others</li>
+            </ul>
+            We will not sell, rent, or otherwise transfer personal information to any third party for commercial purposes without the user's explicit consent.
+          </p>
+          <p className="text-black text-[19.35px] lg:text-[24.7907px] leading-[29px] lg:leading-[37px] font-[400] font-regular"><span className="ml-[15px]">4. Security of Personal Information</span> <br />We take appropriate technical and organizational measures to protect the personal information of our users against unauthorized access, disclosure, alteration, or destruction.</p>
+          <p className="text-black text-[19.35px] lg:text-[24.7907px] leading-[29px] lg:leading-[37px] font-[400] font-regular"><span className="ml-[15px]">5. Data Retention</span> <br />We will retain the personal information of our users for as long as necessary to provide our services, comply with legal requirements, and enforce our terms and conditions.</p>
+          <p className="text-black text-[19.35px] lg:text-[24.7907px] leading-[29px] lg:leading-[37px] font-[400] font-regular"><span className="ml-[15px]">6. Rights and Choices</span> <br />Our users have the right to access, correct, update, or delete their personal information at any time. They also have the right to object to the processing of their personal information and to request the transfer of their personal information to another organisation.</p>
+          <p className="text-black text-[19.35px] lg:text-[24.7907px] leading-[29px] lg:leading-[37px] font-[400] font-regular"><span className="ml-[15px]">7. Changes to Privacy Policy</span> <br />We may update this Privacy Policy from time to time to reflect changes in our practices or to comply with legal requirements. If we make any material changes to this Privacy Policy, we will notify our users by email or by posting a notice on our website.</p>
+          <p className="text-black text-[19.35px] lg:text-[24.7907px] leading-[29px] lg:leading-[37px] font-[400] font-regular"><span className="ml-[15px]">8. Contact Us</span> <br />If you have any questions or concerns about this Privacy Policy, please contact us through our report page <span className="underline">here.</span></p>
+        </div>
+      </div>
+    </section>
   )
 }
 
