d00a56b749be124e85802c328f3147097c57e2fe
oldhand7 <oldhanddev@gmail.com>
2023-04-06 21:09:21 -0400

    add symbols to possible positions

--- src/pages/Generate/Generate.tsx
+++ src/pages/Generate/Generate.tsx
@@ -18,24 +18,37 @@ const Generate = () => {
         notify("This is username")
       } else if (inputValue.trim().split(" ").length > 1) { 
         notify("This is Full Name")
-        const nameArray = inputValue.trim().split(/\s+/); // split by one or more whitespace characters
+        const nameArray = inputValue.trim().split(/\s+/);
+        console.log("nameArray", nameArray)
         let firstName = nameArray[0];
         let lastName = nameArray[1];
-        for (let i = 0; i < firstName.length; i++) {
-          if (!/^[a-zA-Z]$/.test(firstName[i])) {
-            firstName = firstName.replace(firstName[i], t(firstName[i]));
-          }         
-        }   
-        for (let i = 0; i < lastName.length; i++) {
-          if (!/^[a-zA-Z]$/.test(lastName[i])) {
-            lastName = lastName.replace(lastName[i], t(lastName[i]));
-          }         
-        }        
-
+        const enNames = nameArray.map((name: string, index: number) => {
+          for (let i = 0; i < name.length; i++) {
+            if (!/^[a-zA-Z]$/.test(name[i])) {              
+              name = name.replace(name[i], t(name[i]));
+            }         
+          }  
+          return name;
+        })
+        
+        // for (let i = 0; i < firstName.length; i++) {
+        //   if (!/^[a-zA-Z]$/.test(firstName[i])) {
+        //     firstName = firstName.replace(firstName[i], t(firstName[i]));
+        //   }         
+        // }   
+        // for (let i = 0; i < lastName.length; i++) {
+        //   if (!/^[a-zA-Z]$/.test(lastName[i])) {
+        //     lastName = lastName.replace(lastName[i], t(lastName[i]));
+        //   }         
+        // }        
+        
         const separators: string[] = ['', '.', '_'];
-        const resultArray: string[] = ([] as string[]).concat(...separators.map((separator: string): string[] => generateUsername(firstName, lastName, separator)));
-        const reverseArray: string[] = ([] as string[]).concat(...separators.map((separator: string): string[] => generateUsername(lastName, firstName, separator)));
-        let finalResult = [...resultArray, ...reverseArray]
+        // const resultArray: string[] = ([] as string[]).concat(...separators.map((separator: string): string[] => generateUsername(firstName, lastName, separator)));
+        // const reverseArray: string[] = ([] as string[]).concat(...separators.map((separator: string): string[] => generateUsername(lastName, firstName, separator)));
+        // let finalResult = [...resultArray, ...reverseArray]
+        
+        const finalResult: string[] = ([] as string[]).concat(...separators.map((separator: string): string[] => generateUsername(enNames, separator)));
+        console.log("finalResult", finalResult)
         setResult(finalResult);
         // fetchSocial();
       }   
@@ -45,19 +58,48 @@ const Generate = () => {
   }
 
   const generateUsername = (
-    firstName: string,
-    lastName: string,
+    name: string[],
     symbol: string
   ): string[] => {
-    let temp = [];
-    for (let i = 1; i <= firstName.length; i++) {     
-      for (let j = 1; j <= lastName.length; j++) {
-        let combine1 = `${firstName.slice(0, i).toLowerCase()}${symbol}${lastName.slice(0, j).toLowerCase()}`;
-        temp.push(combine1);
+    let temp: string[] = [];
+    for (let i = 1; i <= name[0].length; i++) {     
+      for (let j = 1; j <= name[1].length; j++) {
+        if (symbol === '') {
+          let combine1 = `${name[0].slice(0, i).toLowerCase()}${symbol}${name[1].slice(0, j).toLowerCase()}`;
+          temp = temp.concat(combine1);
+        } else {
+          let combine1 = [];
+          combine1.push(name[0].slice(0, i).toLowerCase(), name[1].slice(0, j).toLowerCase());
+          temp = temp.concat(generateSymbolNames(combine1, symbol));
+        }        
+       
       }
-    }    
+    }    console.log("temp", temp)
     return temp;
+   
   };  
+
+  const generateSymbolNames = (
+    name: string[],
+    symbol: string
+  ): string[] => {
+    let mergeName = name.join('');
+    let withDashes = [];
+    for (let i = 1; i < Math.pow(2, mergeName.length - 1); i++) {
+      let binary = i.toString(2).padStart(mergeName.length - 1, "0");
+      let combination = mergeName[0];
+    
+      for (let j = 0; j < binary.length; j++) {
+        if (binary[j] === "1") {
+          combination += symbol;
+        }
+        combination += mergeName[j + 1];
+      }
+    
+      withDashes.push(combination);
+    }
+    return withDashes;
+  }
 
   const fetchSocial = async () => { console.log("twitter bear key", process.env.REACT_APP_TWITTER_BEARER_TOKEN)
     try {
@@ -75,6 +117,36 @@ const Generate = () => {
   const notify = (message: string): void => {
     toast(message);
   }
+
+// const numbers: number[] = [1, 2, 3, 4];
+// const results: number[][] = [];
+
+// function generateCombinations(currentCombination: number[], remainingNumbers: number[]): void {
+//   if (remainingNumbers.length === 0) {
+//     results.push(currentCombination);
+//     return;
+//   }
+
+//   for (let i = 0; i < remainingNumbers.length; i++) {
+//     const newCombination = [...currentCombination, remainingNumbers[i]];
+//     const newRemainingNumbers = remainingNumbers.filter((_, index) => index !== i);
+//     generateCombinations(newCombination, newRemainingNumbers);
+//   }
+// }
+
+// generateCombinations([], numbers);
+
+// console.log("generateCombinations", results);
+
+
+// const number = "1234";
+
+
+
+
+// console.log("withDashes", withDashes);
+
+
 
   return (
     <section>
